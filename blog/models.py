38e5a099110368e83e01a2b85a149eb2269dd2c2
priyanka <priyanka.pakhale54@gmail.com>
2019-07-26 07:51:04 +0530

    uploading django files to git

--- blog/models.py
+++ blog/models.py
@@ -24,9 +24,9 @@ class Tournament(models.Model):
 
 
     id = models.AutoField(primary_key=True)
-    platform = models.ChoiceField(choices = PLATFORMS)
+    platform = models.IntegerField(choices = PLATFORMS)
     game_name = models.CharField(max_length=50)
-    game_mode = models.ChoiceField(choices = GAME_MODES)
+    game_mode = models.IntegerField(choices = GAME_MODES)
     entry_fee = models.FloatField(default=0)
     tournament_date = models.DateTimeField()
     registration_end_date = models.DateTimeField()
