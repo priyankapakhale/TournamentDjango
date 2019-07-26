c3b56538608f5ae457c47c2cadb09292ef8f33fb
priyanka <priyanka.pakhale54@gmail.com>
2019-07-27 03:44:03 +0530

    uploading django files to git

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Tournament, User
 
 # Register your models here.
-class TournamentAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Tournament._meta.get_fields()]
+#class TournamentAdmin(admin.ModelAdmin):
+#    list_display = [field.name for field in Tournament._meta.get_fields()]
 
-admin.site.register(Tournament, TournamentAdmin)
+admin.site.register(Tournament)
 admin.site.register(User)
